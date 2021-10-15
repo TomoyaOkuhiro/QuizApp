e01131cd3cb558c29356d5d32ad4591f94900242
TomoyaOkuhiro <77881067+TomoyaOkuhiro@users.noreply.github.com>
2021-10-15 23:49:49 +0900

    2. QとAの正誤判定
    ifを用いた正誤判定。
    クリックでアラートを表示させるイベント発火を実装。

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const question = "タマホームのCMで流れているDeep Purpleの曲は？";
-const answers = ["burn", "Smoke on the Water", "Highway Star", "Black Night"];
-const correct = ["Burn"];
+const answers = ["Burn", "Smoke on the Water", "Highway Star", "Black Night"];
+const correct = "Burn";
 
 document.getElementById("js-question").textContent = question;
 
@@ -9,4 +9,10 @@ document.getElementsByTagName("button")[1].textContent = answers[1];
 document.getElementsByTagName("button")[2].textContent = answers[2];
 document.getElementsByTagName("button")[3].textContent = answers[3];
 
-document.getElementsByTagName("button")[0].textContent = answers[0];
+document.getElementsByTagName("button")[0].addEventListener("click", () => {
+  if (correct === document.getElementsByTagName("button")[0].textContent) {
+    window.alert("正解");
+  } else {
+    window.alert("不正解");
+  }
+});
